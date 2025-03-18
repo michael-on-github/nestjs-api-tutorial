c208f49c2e00f0357c0c5d59889f43140dfab186
Michael <[redacted]>
2025-03-18 13:35:52 +0200

    Use global pipes

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { AuthDto } from './dto';
 
-@Injectable({})
+@Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
 
-  signup() {
+  signup(dto: AuthDto) {
     return { msg: 'I have signed up' };
   }
 
